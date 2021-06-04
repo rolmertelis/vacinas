4370e59fb5c0d78e6bfea9b186c4d035fba74214
rolmertelis <rolmertelis@gmail.com>
2021-06-04 20:15:21 -0300

    Commit do dump do  banco de dados

--- dumpbanco.sql
+++ dumpbanco.sql
@@ -18,6 +18,37 @@ USE `vacinarRolmer`;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `aplicacao`
+--
+
+DROP TABLE IF EXISTS `aplicacao`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `aplicacao` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `usuario_id` int NOT NULL,
+  `dataaplicacao` varchar(45) NOT NULL,
+  `obs` varchar(45) DEFAULT NULL,
+  `vacina_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_aplicacao_usuario_idx` (`usuario_id`),
+  KEY `fk_aplicacao_1_idx` (`vacina_id`),
+  CONSTRAINT `fk_aplicacao_usuario` FOREIGN KEY (`usuario_id`) REFERENCES `usuario` (`id`),
+  CONSTRAINT `fk_aplicacao_vacina` FOREIGN KEY (`vacina_id`) REFERENCES `vacina` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=big5;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `aplicacao`
+--
+
+LOCK TABLES `aplicacao` WRITE;
+/*!40000 ALTER TABLE `aplicacao` DISABLE KEYS */;
+INSERT INTO `aplicacao` VALUES (1,1,'25/05/1964','Uma obs',1),(2,1,'25/05/1965','Outra obs',2);
+/*!40000 ALTER TABLE `aplicacao` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `usuario`
 --
 
@@ -33,7 +64,7 @@ CREATE TABLE `usuario` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `email_UNIQUE` (`email`),
   UNIQUE KEY `cpf_UNIQUE` (`cpf`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=big5;
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=big5;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -42,7 +73,7 @@ CREATE TABLE `usuario` (
 
 LOCK TABLES `usuario` WRITE;
 /*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
-INSERT INTO `usuario` VALUES (1,'joao','joao@gmail.com','18','12345678910'),(2,'jose','jose@gmail.com','25','12345678911'),(3,'maria','maria@gmail.com','30','12345678912'),(4,'Juca','juca@gmail.com','40','12345678913'),(5,'Madalena','madalena@gmail.com','25/05/1974','322211122'),(7,'Marialva','marialva@gmail.com','25/05/1984','322211123'),(8,'Januario','januario@gmail.com','25/05/1994','322211144'),(9,'Luis','luis@gmail.com','25/05/1993','322211155');
+INSERT INTO `usuario` VALUES (1,'joao','joao@gmail.com','18','12345678910'),(2,'jose','jose@gmail.com','25','12345678911'),(3,'maria','maria@gmail.com','30','12345678912'),(4,'Juca','juca@gmail.com','40','12345678913'),(5,'Madalena','madalena@gmail.com','25/05/1974','322211122'),(7,'Marialva','marialva@gmail.com','25/05/1984','322211123'),(8,'Januario','januario@gmail.com','25/05/1994','322211144'),(9,'Luis','luis@gmail.com','25/05/1993','322211155'),(13,'Luis','abigobaldo@gmail.com','25/05/1993','7777789652'),(14,'Outro usuario','outrousuario@gmail.com','25/05/1993','11111111115'),(15,'scorpion renew','scorpion@gmail.com','25/05/1993','11111111118'),(19,'jacobino','jacobino@gmail.com','25/05/1993','11111111117'),(20,'juliete mexida','juliete@gmail.com','25/05/1984','11111111114'),(22,'Jamelina','Jamelina@gmail.com','25/05/1984','11111111121'),(24,'siberia','siberia@gmail.com','25/05/1993','11111111129');
 /*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -63,7 +94,7 @@ CREATE TABLE `vacina` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `nome_UNIQUE` (`nome`),
   UNIQUE KEY `sigla_UNIQUE` (`sigla`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=big5;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=big5;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -75,37 +106,6 @@ LOCK TABLES `vacina` WRITE;
 INSERT INTO `vacina` VALUES (1,'Febre Amarela','FBA',60,2,30),(2,'Tetano','TTN',36,1,0),(3,'Amarelona','AMR',60,2,15);
 /*!40000 ALTER TABLE `vacina` ENABLE KEYS */;
 UNLOCK TABLES;
-
---
--- Table structure for table `vacinacao`
---
-
-DROP TABLE IF EXISTS `vacinacao`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `vacinacao` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `usuario` int NOT NULL,
-  `vacina` int NOT NULL,
-  `dose` int NOT NULL,
-  `dataaplicacao` varchar(10) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id_UNIQUE` (`id`),
-  KEY `fk_usuario_idx` (`usuario`),
-  KEY `fk_vacina_idx` (`vacina`),
-  CONSTRAINT `fk_usuario` FOREIGN KEY (`usuario`) REFERENCES `usuario` (`id`),
-  CONSTRAINT `fk_vacina` FOREIGN KEY (`vacina`) REFERENCES `vacina` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=big5;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `vacinacao`
---
-
-LOCK TABLES `vacinacao` WRITE;
-/*!40000 ALTER TABLE `vacinacao` DISABLE KEYS */;
-/*!40000 ALTER TABLE `vacinacao` ENABLE KEYS */;
-UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -116,4 +116,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-05-23 20:26:00
+-- Dump completed on 2021-06-04 20:13:42
